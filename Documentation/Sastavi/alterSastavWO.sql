a86e8ff9bc6aacd3595401b26c366e2bb1bce45e
Nikola Bresliev <nikola.bresliev@gmail.com>
2016-03-13 22:31:53 +0100

    derezervacija, korpa css

--- Documentation/Sastavi/alterSastavWO.sql
+++ Documentation/Sastavi/alterSastavWO.sql
@@ -16,3 +16,21 @@ alter table wo_rezervacija add compositearticle number;
 alter table wo_rezervacija add compositearticlequantity number;
 alter table wo_rezervacija add compositearticlepackquantity number;
 alter table wo_rezervacija add compositearticleunitofmeasure number;
+alter table wo_rezervacija add compositearticleprice number;
+
+CREATE SEQUENCE WO_SEQ_SASTAV_REZERVACIJA_ID;
+
+CREATE TABLE wo_rezervacija_sastava
+    (wo_rezervacija_id              NUMBER,
+    proizvod#                      NUMBER,
+    id_skladista                   NUMBER,
+    kolicina                       NUMBER,
+    id_jedinice_mere               NUMBER,
+    cena                           NUMBER,
+    rabat                          NUMBER,
+    ekstra_rabat                   NUMBER,
+    kolpopakovanju                 NUMBER);
+    
+alter table wo_rezervacija_sastava add id number primary key;
+
+alter table wo_rezervacija_sastava add KOLICINA_UGRADNJE number;
